ab27b33d66f1299f92c13565d1808f1057b44fcb
Alexandru Tarna <tarna.alexandru@gmail.com>
2018-07-01 23:18:05 +0200

    for submission

--- s251897.sql
+++ s251897.sql
@@ -1,97 +1,75 @@
-
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 01, 2018 at 11:15 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `bookings`
+-- Database: `s251897`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `booking`
---
-
-CREATE TABLE `booking` (
-  `user_id` int(11) NOT NULL,
-  `start_addr` int(3) DEFAULT NULL,
-  `stop_addr` int(3) DEFAULT NULL,
-  `passengers` int(3) DEFAULT NULL,
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `booking`
---
-
-INSERT INTO `booking` (`user_id`, `starttime`, `endtime`, `bookedtime`, `id`) VALUES
-(218, 840, 867, 30, 52),
-(219, 867, 975, 120, 53),
-(220, 975, 1020, 50, 54);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `bookings`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `bookings`;
+CREATE TABLE IF NOT EXISTS `bookings` (
+  `booking_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) NOT NULL,
+  `departure` varchar(30) NOT NULL,
+  `destination` varchar(30) NOT NULL,
+  `nr_passengers` int(11) NOT NULL,
+  PRIMARY KEY (`booking_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=67 ;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `bookings`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(218, 'u1@p.it', 'ec6ef230f1828039ee794566b9c58adc'),
-(219, 'u2@p.it', '1d665b9b1467944c128a5575119d1cfd'),
-(220, 'u3@p.it', '7bc3ca68769437ce986455407dab2a1f'),
-(221, 'u4@p.it', '13207e3d5722030f6c97d69b4904d39d');
+INSERT INTO `bookings` (`booking_id`, `username`, `departure`, `destination`, `nr_passengers`) VALUES
+(62, 'u1@p.it', 'AA', 'DD', 2),
+(63, 'u2@p.it', 'BB', 'EE', 1),
+(64, 'u3@p.it', 'DD', 'EE', 1),
+(65, 'u4@p.it', 'AL', 'DD', 1),
+(66, 'u1@p.it', 'FF', 'KK', 4);
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Table structure for table `users`
 --
 
---
--- Indexes for table `booking`
---
-ALTER TABLE `booking`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `user_id` (`user_id`);
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Dumping data for table `users`
 --
 
---
--- AUTO_INCREMENT for table `booking`
---
-ALTER TABLE `booking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=221;COMMIT;
+INSERT INTO `users` (`uid`, `username`, `password`) VALUES
+(10, 'u1@p.it', 'ec6ef230f1828039ee794566b9c58adc'),
+(11, 'u2@p.it', '1d665b9b1467944c128a5575119d1cfd'),
+(12, 'u3@p.it', '7bc3ca68769437ce986455407dab2a1f'),
+(14, 'u4@p.it', '13207e3d5722030f6c97d69b4904d39d');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
